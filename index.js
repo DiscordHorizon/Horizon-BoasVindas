12c130cb36b351b63d3adb705b99a1a88d945bdd
Matheus Ishiyama <semrumo3@hotmail.com>
2021-03-11 12:40:15 -0300

    remove users count

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const Discord = require("discord.js");
-const { discord, guild } = require("./utils/horizonUtils");
-const { users } = require("./include/users");
+const { discord } = require("./utils/horizonUtils");
 const { welcome } = require("./include/welcome");
 
 const bot = new Discord.Client();
@@ -13,17 +12,11 @@ bot.on("ready", async () => {
             url: "https://twitch.tv/bravanzin",
         },
     });
-    users(bot.guilds.cache.get(guild));
     console.log("[Bot] Connected");
 });
 
 bot.on("guildMemberAdd", (user) => {
-    users(user.guild);
     welcome(user);
-});
-
-bot.on("guildMemberRemove", (user) => {
-    users(user.guild);
 });
 
 bot.login(discord);
