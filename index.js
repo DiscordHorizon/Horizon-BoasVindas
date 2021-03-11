63e005bd80a8cf64c343d84e67764500a90cf324
Matheus Ishiyama <semrumo3@hotmail.com>
2021-03-11 12:30:56 -0300

    update voice channel when bots turn on

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 const Discord = require("discord.js");
-const { discord } = require("./utils/horizonUtils");
+const { discord, guild } = require("./utils/horizonUtils");
 const { users } = require("./include/users");
 const { welcome } = require("./include/welcome");
 
 const bot = new Discord.Client();
 
-bot.on("ready", () => {
+bot.on("ready", async () => {
+    await bot.user.setPresence({
+        activity: {
+            name: `Seja bem vindo ao Horizon!!`,
+            type: 1,
+            url: "https://twitch.tv/bravanzin",
+        },
+    });
+    users(bot.guilds.cache.get(guild));
     console.log("[Bot] Connected");
 });
 
