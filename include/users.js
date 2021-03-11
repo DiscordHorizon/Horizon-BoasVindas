97ec5f47ae2545982421a9b2fa6c3cffbfda45df
Matheus Ishiyama <semrumo3@hotmail.com>
2021-03-11 12:39:03 -0300

    delete users count

--- include/users.js
+++
@@ -1,8 +0,0 @@
-const { channels } = require('../utils/horizonUtils');
-
-module.exports = {
-    async users(guild) {
-        const channel = await guild.channels.cache.get(channels.users);
-        channel.setName(`Horizon Members: ${guild.memberCount}`);
-    }
-}
