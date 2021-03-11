88489620a5b7f2a78aaae69f233581545f794f9e
Matheus Ishiyama <semrumo3@hotmail.com>
2021-03-11 12:46:03 -0300

    require server

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const Discord = require("discord.js");
 const { discord } = require("./utils/horizonUtils");
 const { welcome } = require("./include/welcome");
 
+require('./server');
+
 const bot = new Discord.Client();
 
 bot.on("ready", async () => {
