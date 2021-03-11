51286cc304d810ba617fbc0c25bd5f141107d8fc
Matheus Ishiyama <semrumo3@hotmail.com>
2021-03-11 09:54:41 -0300

    require welcome.js

---
+++ index.js
@@ -0,0 +1,19 @@
+const discord = require('discord.js');
+const { config } = require('./config/index');
+const { welcome } = require('./include/welcome');
+
+const bot = new discord.Client();
+
+bot.on('ready', () => {
+    console.log('[Bot] Connected');
+});
+
+bot.on('message', message => {
+    if (message.author.bot) return;
+    if (message.content === '!bravanzin') {
+        welcome(message);
+        //console.log(message.author)
+    }
+})
+
+bot.login(config.discord);
