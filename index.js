e9263c8ef044327044ccbe18b163c3c8ffbba52e
Matheus Ishiyama <semrumo3@hotmail.com>
2021-03-11 12:22:07 -0300

    require users count

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
-const Discord = require('discord.js');
-const { discord } = require('./utils/horizonUtils');
-const { welcome } = require('./include/welcome');
+const Discord = require("discord.js");
+const { discord } = require("./utils/horizonUtils");
+const { users } = require("./include/users");
+const { welcome } = require("./include/welcome");
 
 const bot = new Discord.Client();
 
-bot.on('ready', () => {
-    console.log('[Bot] Connected');
+bot.on("ready", () => {
+    console.log("[Bot] Connected");
 });
 
-bot.on('guildMemberAdd', user => {
+bot.on("guildMemberAdd", (user) => {
+    users(user.guild);
     welcome(user);
+});
+
+bot.on("guildMemberRemove", (user) => {
+    users(user.guild);
 });
 
 bot.login(discord);
